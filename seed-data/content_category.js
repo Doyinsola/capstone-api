548b33b2347d1a878ac057ce6956c287630f39eb
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-06-09 13:48:16 -0400

    added more data to seeds

--- seed-data/content_category.js
+++ seed-data/content_category.js
@@ -15,4 +15,28 @@ module.exports = [
         content_id: 2,
         category_id: 3
     },
+    {
+        content_id: 3,
+        category_id: 1
+    },
+    {
+        content_id: 4,
+        category_id: 1
+    },
+    {
+        content_id: 5,
+        category_id: 1
+    },
+    {
+        content_id: 5,
+        category_id: 4
+    },
+    {
+        content_id: 5,
+        category_id: 3
+    },
+    {
+        content_id: 6,
+        category_id: 5
+    },
 ]
